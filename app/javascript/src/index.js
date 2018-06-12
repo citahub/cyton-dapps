a77c2090073c45dea6bb8e5c1938e75b6d0dd73f
Yao <GyonGyon@users.noreply.github.com>
2018-06-12 11:06:33 +0800

    update: split utils, set default view add interface: remove, add

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,52 +1,15 @@
+import {
+    log,
+    _e,
+    _es,
+    bind,
+    unbind,
+    localStoreParsed,
+    localStoreStore,
+} from './utils.js'
 import './stylesheets/application.scss'
 
-
-const log = console.log.bind(console)
-
-const bind = (element, eventName, callback) => {
-    element.addEventListener(eventName, callback)
-}
-
-const unbind = (element, eventName, callback) => {
-    element.removeEventListener(eventName, callback)
-}
-
-const _e = (selector, element = document) => element.querySelector(selector)
-
-const _es = (selector, element = document) => element.querySelectorAll(selector)
-
 const GlobalTable = {}
-
-const initElementTable = () => {
-    const header = _e('#id_header')
-    const localViewContainer = _e('#id_localViewContainer')
-    const searchBar = _e('#id_searchBar')
-    const navList = _es('.headerNavItem', header)
-
-    GlobalTable.elementTable = {
-        header,
-        localViewContainer,
-        searchBar,
-        navList,
-    }
-}
-
-// const renderTable = {}
-
-// const setOneActive = (target, elementList, classNormal, classActive) => {
-//     const cls = target.className
-//     const clsn = classNormal
-//     if (cls === clsn) {
-//         const clsa = classActive
-//         const l = GlobalTable.elementTable.navList
-//         const len = l.length
-//         for (let i = 0; i < len; i++) {
-//             const e = l[i]
-//             e.className = clsn
-//         }
-//         target.className = clsa
-//     }
-// }
 
 const setFirstActive = (elementList, classActive) => {
     const clsa = classActive
@@ -71,32 +34,8 @@ const setActiveNav = () => {
 
 // with native
 const startNativePage = () => {
-    log('app hybrid')
     appHybrid.startAddWebsitePage()
 }
-
-const bindEvents = () => {
-    const {searchBar} = GlobalTable.elementTable
-    // bind(searchBar, 'focus', startNativePage)
-    bind(searchBar, 'click', startNativePage)
-}
-
-
-// const bindHeader = () => {
-//     const header = GlobalTable.elementTable.header
-//     bind(header, 'click', (event) => {
-//         const target = event.target
-//         const block = target.dataset.block
-//         log(block)
-//         if (block) {
-//             const cls = target.className
-//             const clsn = 'headerNavItem'
-//             const clsa = 'headerNavItem active'
-//             const l = GlobalTable.elementTable.navList
-//             setOneActive(target, l, clsn, clsa)
-//         }
-//     })
-// }
 
 const tDappListCell = (img, name, url) => {
     const t = `<div class="dappsListItem">
@@ -122,53 +61,103 @@ const tMyDapps = (dappInfoList) => {
     return t
 }
 
-const renderMyDappsContainer = () => {
-    let l = localStorage.__viewInfoList_myDapps
-    l = JSON.parse(l)
+const tMyDappsDefault = () => {
+    const t = `<div class="noData">
+                <img class="noDataImg" src="" alt="">
+                <span class="noDataText">无数据显示</span>
+              </div>`
+    return t
+}
+
+const renderMyDappsContainer = (dapps) => {
     const container = GlobalTable.elementTable.localViewContainer
-    if (l && l.length > 0) {
-        container.innerHTML = tMyDapps(l)
+    if (Array.isArray(dapps) && dapps.length > 0) {
+        container.innerHTML = tMyDapps(dapps)
+    } else {
+        container.innerHTML = tMyDappsDefault()
+    }
+}
+
+const mydappSave = (dapps) => {
+    renderMyDappsContainer(dapps)
+    localStoreStore('__viewInfoList_myDapps', dapps)
+}
+
+const mydappAdd = (dapp) => {
+    const dapps = localStoreParsed('__viewInfoList_myDapps')
+    const len = dapps.length
+    for (let i = 0; i < dapps.length; i++) {
+        const obj = dapps[i];
+        if (dapp.url === obj.url) {
+            return
+        }
+    }
+    dapps.push(dapp)
+    mydappSave(dapps)
+}
+
+const mydappRemove = (dapp) => {
+    const dapps = localStoreParsed('__viewInfoList_myDapps')
+    const len = dapps.length
+    for (let i = 0; i < dapps.length; i++) {
+        const obj = dapps[i];
+        if (dapp.url === obj.url) {
+            dapps.splice(i, 1)
+            mydappSave(dapps)
+            return
+        }
     }
 }
 
 const renderMyDapps = () => {
     if (location.pathname === '/dapps/mine') {
-        // test()
-        renderMyDappsContainer()
+        const dapps = localStoreParsed('__viewInfoList_myDapps')
+        renderMyDappsContainer(dapps)
     }
 }
 
-// const test = () => {
-//     const img = 'http://p1.music.126.net/sr9yP4Kt4xxYap5T7CbMqQ==/109951162955032377.jpg?param=180y180'
-//     const name = 'Yuki'
-//     const url = 'http://p1.music.126.net/sr9yP4Kt4xxYap5T7CbMqQ==/109951162955032377.jpg?param=180y180'
-//     const l = [
-//         {
-//             img,
-//             name,
-//             url,
-//         },
-//         {
-//             img,
-//             name,
-//             url,
-//         },
-//         {
-//             img,
-//             name,
-//             url,
-//         },
-//         {
-//             img,
-//             name,
-//             url,
-//         },
-//     ]
-//     localStorage.__viewInfoList_myDapps = JSON.stringify(l)
-// }
+const test = () => {
+    const img = 'http://p1.music.126.net/sr9yP4Kt4xxYap5T7CbMqQ==/109951162955032377.jpg?param=180y180'
+    const name = 'Yuki'
+    const url = 'http://p1.music.126.net/sr9yP4Kt4xxYap5T7CbMqQ==/109951162955032377.jpg?param=180y180'
+    const info = {
+        img,
+        name,
+        url,
+    }
+    const l = [info, info, info]
+    localStoreStore('__viewInfoList_myDapps', l)
+    setInterval(() => {
+        mydappRemove(info)
+        setTimeout(() => {
+            mydappAdd(info)
+        }, 1500)
+    }, 3000)
+}
+
+
+const bindEvents = () => {
+    const {searchBar} = GlobalTable.elementTable
+    bind(searchBar, 'click', startNativePage)
+}
+
+const initElementTable = () => {
+    const header = _e('#id_header')
+    const localViewContainer = _e('#id_localViewContainer')
+    const searchBar = _e('#id_searchBar')
+    const navList = _es('.headerNavItem', header)
+
+    GlobalTable.elementTable = {
+        header,
+        localViewContainer,
+        searchBar,
+        navList,
+    }
+}
 
 const init = () => {
     initElementTable()
+    // test()
     bindEvents()
     setActiveNav()
     renderMyDapps()
@@ -179,9 +168,3 @@ const main = () => {
 }
 
 main()
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.getElementById('searchBar').addEventListener('focus',
-//         startNativePage(),
-//     )
-// })
