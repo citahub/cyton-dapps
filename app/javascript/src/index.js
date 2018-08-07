08b614efaf4b6e335d5d0e3634d4dfbc825113fa
Yao <GyonGyon@users.noreply.github.com>
2018-08-07 21:12:52 +0800

    update: split code api for outer

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -2,6 +2,7 @@ import { log, _e, _es, bind, setStyle, setStyles, localStoreParsed, localStoreSt
 import { GlobalTable, initElementTable } from './global'
 import { renderMydapps } from './dappsRender'
 import createHeaderAnime from './headerAnime'
+import { initApi} from './apiOuter'
 import './imgs/index'
 import './stylesheets'
 
@@ -39,98 +40,6 @@ const startNativePage = (event) => {
     log(err)
   }
   event.stopPropagation()
-}
-
-const mydappSave = (dapps) => {
-  renderMydapps(dapps)
-  localStoreStore('__viewInfoList_mydapps', dapps)
-}
-
-const mydappLoaded = () => {
-  let dapps = localStoreParsed('__viewInfoList_mydapps')
-  if (!Array.isArray(dapps)) {
-    dapps = []
-  }
-  return dapps
-}
-
-// const mydappLoaded = () => {
-//   let dapps = localStoreParsed('__viewInfoList_mydapps')
-//   if (!Array.isArray(dapps)) {
-//     dapps = []
-//   }
-//   return dapps
-// }
-
-const mydappAdd = (dapp) => {
-  const { entry, icon, name } = dapp
-  const o = {
-    img: icon,
-    url: entry,
-    name: name,
-  }
-  const dapps = mydappLoaded()
-  const len = dapps.length
-  for (let i = 0; i < dapps.length; i++) {
-    const obj = dapps[i]
-    if (o.url === obj.url) {
-      return
-    }
-  }
-  dapps.push(o)
-  mydappSave(dapps)
-}
-
-const myhistoryAdd = (dapp) => {
-  const { entry, icon, name } = dapp
-  const o = {
-    img: icon,
-    url: entry,
-    name: name,
-  }
-  const dapps = mydappLoaded()
-  const len = dapps.length
-  for (let i = 0; i < len; i++) {
-    const obj = dapps[i]
-    if (o.url === obj.url) {
-      return
-    }
-  }
-  dapps.push(o)
-  // log('dapp added', dapps)
-  mydappSave(dapps)
-}
-
-const mydappRemove = (url) => {
-  const dapps = mydappLoaded()
-  const len = dapps.length
-  for (let i = 0; i < len; i++) {
-    const obj = dapps[i]
-    if (url === obj.url) {
-      dapps.splice(i, 1)
-      mydappSave(dapps)
-      return
-    }
-  }
-}
-
-// 提供给外部的接口
-const initApi = () => {
-  if (window.__mydapp === undefined) {
-    window.__mydapp = {
-      remove: mydappRemove,
-      add: mydappAdd,
-    }
-    Object.freeze(window.__mydapp)
-  }
-
-  if (window.__myhistory === undefined) {
-    window.__myhistory = {
-      remove: mydappRemove,
-      add: mydappAdd,
-    }
-    Object.freeze(window.__mydapp)
-  }
 }
 
 const test = () => {
