e4388248954a322e42f4c3661fd7572b7914f0e9
Yao <GyonGyon@users.noreply.github.com>
2018-08-07 19:45:08 +0800

    update: add utils & format code
    setStyle, setStyles, binds,

--- app/javascript/src/utils.js
+++ app/javascript/src/utils.js
@@ -1,42 +1,50 @@
 let _log = () => {
-    return () => {}
-    return console.log.bind(console, '>>>')
+  // return () => {}
+  return console.log.bind(console, '>>>')
 }
 const log = _log()
 
 const localStoreParsed = (key) => {
-    const json = localStorage.getItem(key)
-    if (json) {
-        const value = JSON.parse(json)
-        return value
-    } else {
-        return null
-    }
+  const json = localStorage.getItem(key)
+  if (json) {
+    const value = JSON.parse(json)
+    return value
+  } else {
+    return null
+  }
 }
 
 const localStoreStore = (key, value) => {
-    const json = JSON.stringify(value)
-    localStorage.setItem(key, json)
+  const json = JSON.stringify(value)
+  localStorage.setItem(key, json)
 }
 
 const bind = (element, eventName, callback) => {
-    element.addEventListener(eventName, callback)
+  element.addEventListener(eventName, callback)
+}
+
+const binds = (elements, eventName, callback) => {
+  elements.forEach((element) => {
+    bind(element, eventName, callback)
+  })
 }
 
 const unbind = (element, eventName, callback) => {
-    element.removeEventListener(eventName, callback)
+  element.removeEventListener(eventName, callback)
 }
 
 const _e = (selector, element = document) => element.querySelector(selector)
 
 const _es = (selector, element = document) => element.querySelectorAll(selector)
 
-export {
-    log,
-    _e,
-    _es,
-    bind,
-    unbind,
-    localStoreParsed,
-    localStoreStore,
+const setStyle = (element, style) => {
+  Object.keys(style).forEach((key) => {
+    element.style[key] = style[key]
+  })
 }
+
+const setStyles = (elements, style) => {
+  elements.forEach((element) => setStyle(element, style))
+}
+
+export { log, _e, _es, bind, binds, unbind, setStyle, setStyles, localStoreParsed, localStoreStore }
