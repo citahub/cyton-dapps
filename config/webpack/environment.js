074145f71d5df5e922d17106f675b2679904eab4
Yao <yao@cryptape.com>
2018-09-11 14:16:50 +0800

    update: add jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,21 @@
 const { environment } = require('@rails/webpacker')
 
 // resolve-url-loader must be used before sass-loader
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+  })
+)
+
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader',
   options: {
-    attempts: 1
-  }
-});
+    attempts: 1,
+  },
+})
 
 module.exports = environment
