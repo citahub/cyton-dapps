be7af8aa618a65ec174eb33d54474c20f4408b5c
Yao <yao@cryptape.com>
2018-09-11 14:21:23 +0800

    update: add info more mine history route

--- app/controllers/dapps_controller.rb
+++ app/controllers/dapps_controller.rb
@@ -4,7 +4,16 @@ class DappsController < ApplicationController
     @dapps = Dapp.all.group_by { |dapp| dapp.d_type }
   end
 
+  def info
+  end
+
+  def more
+  end
+
   def mine
+  end
+
+  def history
   end
 
 end
