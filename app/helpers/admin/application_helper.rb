8b1f4302c56bf3b16ad4e2bfc11112bbcf7c2038
classicalliu <classicalliu@gmail.com>
2018-09-13 00:27:41 +0800

    add material design style ui to admin

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,6 +1,20 @@
 module Admin::ApplicationHelper
-  def link_to_if_exist(url)
+  def link_to_if_exist(url, options = {})
     return url if url.nil?
-    link_to url, url, target: "_blank"
+    link_to url, url, target: "_blank", **options
+  end
+
+  def page_title(title = '')
+    base_title = "DApp Admin"
+    return base_title if title.blank?
+    title
+  end
+
+  def handle_image_url(url, options = {})
+    link_to url, target: "_blank" do
+      image_tag url, **options
+    end
+  rescue
+    url
   end
 end
