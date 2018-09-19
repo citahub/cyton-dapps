2518d631fbde932654c6bbdfa30ada412a59d562
Yao <yao@cryptape.com>
2018-09-19 21:32:49 +0800

    update: set seeds functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,83 +18,90 @@ dapp_types = [
   { name: "其它" }
 ]
 
-start_at = Time.now
-end_at = Time.now + 1.day
+def start_at()
+  Time.now
+end
 
-dapps = [
-  {
-    name: "First Forever",
+def end_at()
+  Time.now + 1.year
+end
+
+def create_dapp(dapps, type)
+  normal = {
+    dapp_type_id: type.id, 
+    start_at: start_at, 
+    end_at: end_at, 
+    desc: '全球潮流音乐，搭配舞蹈、表演等内容形式，还有超多原创特效、滤镜、场景切换帮你一秒变大片，为你打造刷爆朋友圈的魔性短视频。脑洞有多大，舞台就有多大！好玩的人都在这儿！', 
+    score: 1, 
+    developer: 'developer', 
+    marketing_url: 'https://dapp.cryptape.com/faucet/', 
+    intro: '东半球最最好玩的游戏',
     url_address: "https://dapp.cryptape.com/demo/first-forever",
     d_type: :new_dapp,
     logo_url: "https://cdn.cryptape.com/neuron/Artboard3.png",
     ios_version: "0.0.0.000000",
     android_version: "0.0.0.000000",
+  }
+  Dapp.create(dapps.map { |n| normal.merge(n) })
+end
+
+def create_banner(banners)
+  normal = {
+    start_at: start_at, 
+    end_at: end_at,
+    image_url: "https://img.chainnews.com/material/images/f15b61d0a3256e8af33eaebadc735cba.jpg",
+    ios_version: "0.0.0.000000",
+    android_version: "0.0.0.000000",
+  }
+  
+  Banner.create!(banners.map { |n| normal.merge(n) })
+end
+
+dapps = [
+  {
+    name: "First Forever",
+    url_address: "https://dapp.cryptape.com/demo/first-forever",
+    logo_url: "https://cdn.cryptape.com/neuron/Artboard3.png",
+    ios_version: "1.0.0.000000",
+    android_version: "1.0.0.000000",
   },
   {
     name: "AppChain Testnet Faucet",
     url_address: "https://dapp.cryptape.com/faucet",
-    d_type: :new_dapp,
     logo_url: "https://avatars1.githubusercontent.com/u/35361817",
-    ios_version: "0.0.0.000000",
-    android_version: "0.0.0.000000",
+    ios_version: "2.0.0.000000",
+    android_version: "2.0.0.000000",
   },
   {
     name: "Hyperdragon",
     url_address: "https://hyperdragons.alfakingdom.com/",
-    d_type: :popular,
     logo_url: "https://hyperdragons.alfakingdom.com/favicon.ico",
-    ios_version: "0.0.0.000000",
-    android_version: "0.0.0.000000",
+    ios_version: "3.0.0.000000",
+    android_version: "3.0.0.000000",
   },
   {
     name: "DApp Review",
     url_address: "https://dapp.review/explore",
-    d_type: :popular,
     logo_url: "https://dapp.review/favicon.ico",
-    ios_version: "0.0.0.000000",
-    android_version: "0.0.0.000000",
   }
 ]
 
 banners = [
-  {
-    image_url: "https://img.chainnews.com/material/images/f15b61d0a3256e8af33eaebadc735cba.jpg",
-    ios_version: "0.0.0.000000",
-    android_version: "0.0.0.000000",
-  },
-  {
-    image_url: "https://img.chainnews.com/material/images/f15b61d0a3256e8af33eaebadc735cba.jpg",
-    ios_version: "0.0.0.000000",
-    android_version: "0.0.0.000000",
-  },
-  {
-    image_url: "https://img.chainnews.com/material/images/f15b61d0a3256e8af33eaebadc735cba.jpg",
-    ios_version: "0.0.0.000000",
-    android_version: "0.0.0.000000",
-  },
-  {
-    image_url: "https://img.chainnews.com/material/images/f15b61d0a3256e8af33eaebadc735cba.jpg",
-    ios_version: "0.0.0.000000",
-    android_version: "0.0.0.000000",
-  },
-  {
-    image_url: "https://img.chainnews.com/material/images/f15b61d0a3256e8af33eaebadc735cba.jpg",
-    ios_version: "0.0.0.000000",
-    android_version: "0.0.0.000000",
-  },
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
 ]
 
-dapp_desc = '全球潮流音乐，搭配舞蹈、表演等内容形式，还有超多原创
-    特效、滤镜、场景切换帮你一秒变大片，为你打造刷爆朋友
-    圈的魔性短视频。脑洞有多大，舞台就有多大！好玩的人都
-    在这儿！'
-
-dapp_marketing_url = 'https://dapp.cryptape.com/faucet/'
-
 ApplicationRecord.transaction do
+  create_banner banners
+  
   DappType.create!(dapp_types)
-  Banner.create!(banners.map { |n| n.merge(start_at: start_at, end_at: end_at) })
   popular_type = DappType.find_by(name: "人气推荐")
-  Dapp.create!(dapps.map { |n| n.merge(dapp_type_id: popular_type.id, start_at: start_at, end_at: end_at, desc: dapp_desc, score: 1, developer: 'developer', marketing_url: dapp_marketing_url, intro: '东半球最最好玩的游戏',
-    ) })
+  game_type = DappType.find_by(name: "游戏")
+  create_dapp dapps, popular_type
+  create_dapp dapps, game_type
 end
