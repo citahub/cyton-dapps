25114842d455af173c4c0c3bacb580cb0eae7384
classicalliu <classicalliu@gmail.com>
2018-12-04 14:38:30 +0800

    Set dapp_types i18n

--- app/models/dapp_type.rb
+++ app/models/dapp_type.rb
@@ -2,5 +2,7 @@ class DappType < ApplicationRecord
   has_many :dapps
   validates :name, presence: true, uniqueness: true
 
+  translates :name
+
   scope :default_order, -> { order(id: :asc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_040050) do
+ActiveRecord::Schema.define(version: 2018_12_04_062635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,11 +61,19 @@ ActiveRecord::Schema.define(version: 2018_12_04_040050) do
     t.index ["locale"], name: "index_dapp_translations_on_locale"
   end
 
-  create_table "dapp_types", force: :cascade do |t|
-    t.string "name", null: false
+  create_table "dapp_type_translations", force: :cascade do |t|
+    t.integer "dapp_type_id", null: false
+    t.string "locale", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_dapp_types_on_name", unique: true
+    t.string "name"
+    t.index ["dapp_type_id"], name: "index_dapp_type_translations_on_dapp_type_id"
+    t.index ["locale"], name: "index_dapp_type_translations_on_locale"
+  end
+
+  create_table "dapp_types", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "dapps", force: :cascade do |t|
